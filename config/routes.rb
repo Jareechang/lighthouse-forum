86527fbd40ee3bea485beaa22d2f95ea15e170e9
Jareechang <therealjerrycan@gmail.com>
2015-02-02 14:44:47 +0000

    finished lighthouse forum

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ LighthouseForum::Application.routes.draw do
 
   # Example of regular route:
   LighthouseForum::Application.routes.draw do 
-    get 'posts', to: 'posts#index'
+
+    resources :posts
+    root to: 'posts#index'
   end 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
